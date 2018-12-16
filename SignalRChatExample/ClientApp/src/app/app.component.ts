62c4612bc450a4e2f83adb60ab39f77b2633947d
Robert Szachnowski <rszachnowski@wi.ps.pl>
2018-12-16 21:31:14 +0100

    Switch angular to latest version. Prepare all necessery code.

--- SignalRChatExample/ClientApp/src/app/app.component.ts
+++ SignalRChatExample/ClientApp/src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { HttpClient } from "@angular/common/http";
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"]
 })
 export class AppComponent {
-  title = 'app';
+  title = "ClientApp";
+
+  weatherContent: string = "";
+
+  /**
+   *
+   */
+  constructor(private http: HttpClient) {
+    http
+      .get("api/sampleData/WeatherForecasts")
+      .subscribe(w => (this.weatherContent = JSON.stringify(w)));
+  }
 }
